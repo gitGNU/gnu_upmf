eaa6cf950283f40ef4136dbce9f82caffa008975
Aljosha Papsch <misc@rpapsch.de>
2013-04-27 16:10:43 +0200

    Added devel/patch

---
+++ devel/patch/patch.scm
@@ -0,0 +1,7 @@
+'((type         . draft)
+  (mode         . autotools)
+  (name         . "patch")
+  (section      . "devel")
+  (description  . "A program  for modifying or creating files by applying a “patch” file")
+  (options      . "")
+  (releases     . (("2.7.1" . "http://ftp.gnu.org/gnu/patch/patch-2.7.1.tar.xz"))))

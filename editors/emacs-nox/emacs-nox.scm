b97803a32ff83a81edfc2213503b04f6c27790ff
Aljosha Papsch <misc@rpapsch.de>
2013-04-27 16:35:42 +0200

    Added editors/emacs-nox

---
+++ editors/emacs-nox/emacs-nox.scm
@@ -0,0 +1,8 @@
+'((type         . draft)
+  (mode         . autotools)
+  (name         . "emacs-nox")
+  (section      . "editors")
+  (description  . "Emacs Meta Alt Control Shift")
+  (options      . "--without-all")
+  (releases     . (("24.3" . "http://ftpmirror.gnu.org/emacs/emacs-24.3.tar.xz")))
+  (install-info . #t))

e1c378cf0345afc74f4e9b4e3410563c3e57ecf1
Aljosha Papsch <misc@rpapsch.de>
2013-04-27 16:07:25 +0200

    Added apps/man-db

---
+++ apps/man-db/man-db.scm
@@ -0,0 +1,9 @@
+'((type         . draft)
+  (mode         . autotools)
+  (name         . "man-db")
+  (section      . "apps")
+  (description  . "Programs for finding and viewing man pages")
+  (options      . "--disable-setuid --with-browser=/usr/bin/lynx \\
+                   --with-vgrind=/usr/bin/vgrind \\
+                   --with-grap=/usr/bin/grap")
+  (releases     . (("2.6.3" . "http://download.savannah.gnu.org/releases/man-db/man-db-2.6.3.tar.xz"))))

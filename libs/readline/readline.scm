9aa8efe3e90e173b85db91290b3998ca6ad6e4be
Aljosha Papsch <misc@rpapsch.de>
2013-04-28 19:41:03 +0200

    Add libs/readline

---
+++ libs/readline/readline.scm
@@ -0,0 +1,13 @@
+'((type         . draft)
+  (mode         . autotools)
+  (name         . "readline")
+  (section      . "libs")
+  (description  . "A set of libraries that offers command-line editing and history capabilities")
+  (options      . "")
+  (releases     . (("6.2" . "http://ftp.gnu.org/gnu/readline/readline-6.2.tar.gz")))
+  (install-info . #t)
+  ;; Don't move old libraries to <libraryname>.old. This causes linking bugs sometimes.
+  (pre-hook     . ("sed -i '/MV.*old/d' Makefile.in"
+		   "sed -i '/{OLDSUFF}/c:' support/shlib-install"))
+  (patches      . ((name . "readline-6.2-fixes-1.patch")
+		   (for-version . ("6.2")))))
